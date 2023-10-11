f07e9d9b86d96a6ec19d5a1718f72083bc2d2f27
TristanR8 <146938503+TristanR8@users.noreply.github.com>
2023-10-11 22:25:53 +0700

    rotasi enemy

--- BioStrike - Operation Outbreak/Assets/Script/Gameplay/enemy.cs
+++ BioStrike - Operation Outbreak/Assets/Script/Gameplay/enemy.cs
@@ -8,17 +8,50 @@ public class enemy : MonoBehaviour
     public Transform borderCheck;
     public int enemyHP = 100;
 
+    private Rigidbody2D rb;
+    public float KecepatanGerak;
+    public bool berbalik;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        berbalik = true;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target.position.x > transform.position.x)
+        posisi();
+        Enemyrotasi();
+    }
+
+    private void Enemyrotasi()
+    {
+        if (berbalik)
+        {
+            rb.velocity = new Vector2(KecepatanGerak, rb.velocity.y);
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-KecepatanGerak, rb.velocity.y);
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Balik"))
+        {
+            berbalik =! berbalik;
+        }
+    }
+
+    private void posisi()
+    {
+        if (target.position.x > transform.position.x)
         {
             transform.localScale = new Vector2(1f, 1f);
         }
