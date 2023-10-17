7bc63d5516497f5e7f96e72979430f620a6289ee
TristanR8 <146938503+TristanR8@users.noreply.github.com>
2023-10-17 21:36:01 +0700

    Pause/Resume/Main Menu button

---
+++ BioStrike - Operation Outbreak/Assets/Script/Gameplay/Pausebtn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausebtn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PauseScreen;
+
+    [SerializeField]
+    private GameObject PlayScreen;
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PlayScreen.SetActive(false);
+        PauseScreen.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        PlayScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Lobby");
+    }
+}
